91d7873c5cbdcb518a1d0af96a374f7e99e54e65
manuel <man7castellanos@gmail.com>
2023-06-06 11:55:08 -0400

    Add story editor component

--- src/pages/content-generation/index.jsx
+++ src/pages/content-generation/index.jsx
@@ -1,8 +1,46 @@
+import { useState } from 'react';
+import { Box, TextField, Button } from '@mui/material';
+
 export default function ContentGeneration() {
+  const [story, setStory] = useState('');
+  const handleChange = (e) => {
+    setStory(e.target.value);
+  };
+  const handleSubmit = () => {
+    console.log(story);
+  };
+
   return (
     <>
-      <h1>Here is ContentGeneration Page</h1>
-      <h1>Yes it is</h1>
+      <Box
+        sx={{
+          height: '75%',
+          border: 2,
+          marginBottom: 2,
+          borderRadius: 1,
+          padding: 2,
+        }}
+      >
+        Here is ContentGeneration Page
+      </Box>
+      <Box sx={{ height: '20%' }}>
+        <TextField
+          multiline
+          rows={4}
+          label="Start Writing"
+          value={story}
+          fullWidth
+          onChange={handleChange}
+        />
+        <Button
+          variant="outlined"
+          size="large"
+          sx={{ mt: 1, p: 1 }}
+          onClick={handleSubmit}
+        >
+          SUBMIT
+        </Button>
+      </Box>
     </>
   );
 }
