8c41bde9d25a741eef00000697c5c2274b358ffc
manuel <man7castellanos@gmail.com>
2023-07-05 01:00:39 -0400

    Add auth provider context

--- src/components/explorer/index.jsx
+++ src/components/explorer/index.jsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+
 export default function CommonExplorer({ title, description, children }) {
   return (
     <div className="common-explorer flex flex-col h-full justify-between">
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Box, Grid } from '@mui/material';
+import { useAuth } from 'hooks/useAuth';
 import { Sidenav as Sidebar } from 'layout/Sidebar';
 import Header from 'layout/Header';
 
 export default function Dashboard() {
+  const { user } = useAuth();
+  console.log(user);
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <Box sx={{ flexGrow: 1, height: '100%' }}>
       <Header authenticated={true} />
